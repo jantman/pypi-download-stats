e23b4154d98abb2b64f563ce0f1da3d29e7ad528
Jason Antman <jason@jasonantman.com>
2016-08-13 23:07:04 -0400

    more-specific versions

--- setup.py
+++ setup.py
@@ -44,8 +44,10 @@ with open('README.rst') as file:
 requires = [
     'google-api-python-client>=1.5.0',
     'oauth2client>=3.0.0',
-    'bokeh>=0.12.1',
-    'pandas>=0.16.1'
+    'bokeh==0.12.1',
+    'pandas>=0.16.1',
+    'numpy>=1.6',
+    'tzlocal'
 ]
 
 classifiers = [
